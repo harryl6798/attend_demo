983305aa970ce241b27fbbbb375d9651a337e640
harryl6798 <harryl6798@gmail.com>
2019-04-29 18:58:18 -0500

    UI Design Major Push

---
+++ Attend_Demo/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:the_gorgeous_login/ui/login_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(new MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'TheGorgeousLogin',
+      theme: new ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage(),
+    );
+  }
+}
